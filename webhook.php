e2cdc8a877ee9e0e45ea25ee630509bf89d62048
suzuki <smt.sys-22@sysmet.co.jp>
2018-12-04 16:41:26 +0900

    make it better

--- webhook.php
+++ webhook.php
@@ -1,3 +1,4 @@
+Authorization: Bearer
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('mREkOWlkv8FLyzekkvjSLa7peMd0ZXsQlRlTdfPGf83I1/L8agaoMUkKsmHOdree39U4VwZV4VMfe49EycwgTa9Rg8xlNnk4rGh5jlkZdqidra0OEmPSF0mmwtqHnoBklUUhXzczMEVVXLQeRYQ0IQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => fb18e7478baee5c48f1cfad36786270f']);
