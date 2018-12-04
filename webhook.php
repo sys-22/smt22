3ecb571c91763e81d4bf175f095bd88a84ee301c
suzuki <smt.sys-22@sysmet.co.jp>
2018-12-04 11:28:04 +0900

    make it better

--- webhook.php
+++ webhook.php
@@ -1,74 +1,180 @@
-require_once('./vendor/autoload.php');
 
-new LineMessage;
+ package bot
 
-class LineMessage{
+import (
+	"fmt"
+	"net/http"
+	"os"
 
-  private $token = '9DaHPUurWQB3oZVvk9iVSWatRaTSR/qMBpGMs3HwFzfOkAGXiLOpp1cZs6F2SydS39U4VwZV4VMfe49EycwgTa9Rg8xlNnk4rGh5jlkZdqijpiKGsrCmH/JFY1OXKvgC3WtMfBB+fIF9G0osw3tuLAdB04t89/1O/w1cDnyilFU=';
-  private $secret = 'dd6c195e52c72d80b7c32098843f9aba';
-  private $profile_array = array(); //プロフィールを格納する配列 displayName:表示名 userId:ユーザ識別子 pictureUrl:画像URL statusMessage:ステータスメッセージ
+	"golang.org/x/net/context"
 
-new Bot;
+	"github.com/line/line-bot-sdk-go/linebot"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
+)
 
-class Bot{
-  private $channel_id = "1627227117";
-  private $channel_secret = "dd6c195e52c72d80b7c32098843f9aba";
-  private $mid = "9DaHPUurWQB3oZVvk9iVSWatRaTSR/qMBpGMs3HwFzfOkAGXiLOpp1cZs6F2SydS39U4VwZV4VMfe49EycwgTa9Rg8xlNnk4rGh5jlkZdqijpiKGsrCmH/JFY1OXKvgC3WtMfBB+fIF9G0osw3tuLAdB04t89/1O/w1cDnyilFU=";
-  private $header;
+/**
+ * LINE Botクライアントインスタンスを生成
+ */
+func createBotClient(c context.Context, client *http.Client) (bot *linebot.Client, err error) {
+	var (
+		channelSecret = os.Getenv("dd6c195e52c72d80b7c32098843f9aba")
+		channelToken  = os.Getenv("9DaHPUurWQB3oZVvk9iVSWatRaTSR/qMBpGMs3HwFzfOkAGXiLOpp1cZs6F2SydS39U4VwZV4VMfe49EycwgTa9Rg8xlNnk4rGh5jlkZdqijpiKGsrCmH/JFY1OXKvgC3WtMfBB+fIF9G0osw3tuLAdB04t89/1O/w1cDnyilFU=")
+	)
 
-  private $from;
-  private $text;
-  private $content_type;
-
-  private $name;
-
-  public function __construct(){
-
-    $json_string = file_get_contents('php://input');
-    $receive = json_decode($json_string);
-    $this->from = $receive->result{0}->content->from;
-    $this->text = $receive->result{0}->content->text;
-    $this->content_type = $receive->result[0]->content->contentType;
-
-    $this->header = array(
-      "Content-Type: application/json; charser=UTF-8",
-      "X-Line-ChannelID:" . $this->channel_id,
-      "X-Line-ChannelSecret:" . $this->channel_secret,
-      "X-Line-Trusted-User-With-ACL:" . $this->mid,
-    );
-
-    $this->getting_user_profile_rinformation($this->from);
-    $this->sending_messages($this->name);
-
-  }
-
-  private function sending_messages($message){
-    $url = "https://trialbot-api.line.me/v1/events";
-
-    $data = array("to" => array($this->from), "toChannel" => 1383378250, "eventType" => "138311608800106203", "content" => array("contentType" => 1, "toType" => 1, "text" => $message));
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header);
-    $result = curl_exec($ch);
-    curl_close($ch);
-  }
-
-  private function getting_user_profile_rinformation($mid) {
-    $url = "https://trialbot-api.line.me/v1/profiles?mids={$mid}";
-
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $this->header);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $output = curl_exec($curl);
-    $receive = json_decode($output);
-    error_log($output);
-
-    $this->name = $receive->contacts[0]->displayName; //displayName=>名前 pictureUrl=>プロフィール画像 statusMessage=>自己紹介文
-    $this->name .= "さんこんにちは！";
-
-    file_put_contents("json.php", $output);
-  }
+	bot, err = linebot.New(channelSecret, channelToken, linebot.WithHTTPClient(client)) //Appengineのurlfetchを使用する
+	if err != nil {
+		log.Errorf(c, "Error occurred at create linebot client: %v", err)
+		return bot, err
+	}
+	return bot, nil
 }
+
+/**
+ * Get event sender's id
+ */
+func getSenderID(c context.Context, event *linebot.Event) string {
+	switch event.Source.Type {
+	case linebot.EventSourceTypeGroup:
+		return event.Source.GroupID
+	case linebot.EventSourceTypeRoom:
+		return event.Source.RoomID
+	case linebot.EventSourceTypeUser:
+		return event.Source.UserID
+	}
+	log.Warningf(c, "Can not get sender id. type: %v", event.Source.Type)
+	return ""
+}
+
+/**
+ * 送信者の表示名を取得する
+ *
+ * ユーザしか取得できないので、ルームおよびグループではidをそのまま返す
+ * グループメンバーのUserIDの場合、そのユーザが直接Botと友だち登録していなければ取得できない
+ */
+func getSenderName(c context.Context, bot *linebot.Client, from string) string {
+	if len(from) == 0 {
+		log.Warningf(c, "Parameter `mid` was not specified.")
+		return from
+	}
+	if from[0:1] == "U" {
+		senderProfile, err := bot.GetProfile(from).Do()
+		if err != nil {
+			log.Warningf(c, "Error occurred at get sender profile. from: %v, err: %v", from, err)
+			return from
+		}
+		return senderProfile.DisplayName
+	}
+	return from
+}
+
+/**
+ * LINE Messaging APIからのコールバックをハンドリング
+ */
+func lineBotCallback(w http.ResponseWriter, r *http.Request) {
+
+	c := appengine.NewContext(r)
+	bot, err := createBotClient(c, urlfetch.Client(c))
+	if err != nil {
+		return
+	}
+
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			log.Warningf(c, "Linebot request status: 400")
+			w.WriteHeader(400)
+		} else {
+			log.Warningf(c, "linebot request status: 500\n\terror: %v", err)
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		switch event.Type {
+		case linebot.EventTypeFollow, linebot.EventTypeJoin:
+			sender := getSenderName(c, bot, getSenderID(c, event))
+			message := sender + " さん、友だち登録ありがとうございます！"
+			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message)).Do(); err != nil {
+				log.Errorf(c, "Error occurred at reply-message for follow/join. err: %v", err)
+			}
+
+		case linebot.EventTypeUnfollow, linebot.EventTypeLeave:
+			sender := getSenderName(c, bot, getSenderID(c, event))
+			message := sender + " さん、さようなら"
+			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message)).Do(); err != nil {
+				log.Errorf(c, "Error occurred at reply-message for unfollow/leave. err: %v", err)
+			}
+
+		case linebot.EventTypeMessage:
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				var replyMessage string
+				if message.Text == "/version" {
+					//バージョン
+					replyMessage = "version: " + version
+
+				} else if message.Text == "/mention" || message.Text == "/mention1" {
+					//IDでメンション
+					replyMessage = "@" + event.Source.UserID + " メンションになっていますか？"
+
+				} else if message.Text == "/mention2" {
+					//名前でメンション
+					sender := getSenderName(c, bot, event.Source.UserID)
+					replyMessage = "@" + sender + " メンションになっていますか？"
+
+				} else if message.Text == "/profile" {
+					//ユーザのプロファイルを取得（旧API）
+					senderProfile, err2 := bot.GetProfile(event.Source.UserID).Do()
+					if err2 != nil {
+						replyMessage = fmt.Sprintf("GetProfile()でプロファイルが取得できませんでした\nerr: %v", err2)
+					} else {
+						replyMessage = fmt.Sprintf("UserID: %v\nDisplayName: %v\nPictureURL: %v\nStatusMessage: %v",
+							senderProfile.UserID,
+							senderProfile.DisplayName,
+							senderProfile.PictureURL,
+							senderProfile.StatusMessage,
+						)
+					}
+
+				} else if message.Text == "/profile2" {
+					//ユーザのプロファイルを取得（新API）
+					switch event.Source.Type {
+					case linebot.EventSourceTypeGroup:
+						senderProfile, err2 := bot.GetGroupMemberProfile(event.Source.GroupID, event.Source.UserID).Do()
+						if err2 != nil {
+							replyMessage = fmt.Sprintf("GetGroupMemberProfile()でプロファイルが取得できませんでした\nerr: %v", err2)
+						} else {
+							replyMessage = fmt.Sprintf("UserID: %v\nDisplayName: %v\nPictureURL: %v\nStatusMessage: %v",
+								senderProfile.UserID,
+								senderProfile.DisplayName,
+								senderProfile.PictureURL,
+								senderProfile.StatusMessage,
+							)
+						}
+					case linebot.EventSourceTypeRoom:
+						senderProfile, err2 := bot.GetRoomMemberProfile(event.Source.RoomID, event.Source.UserID).Do()
+						if err2 != nil {
+							replyMessage = fmt.Sprintf("GetRoomMemberProfile()でプロファイルが取得できませんでした\nerr: %v", err2)
+						} else {
+							replyMessage = fmt.Sprintf("UserID: %v\nDisplayName: %v\nPictureURL: %v\nStatusMessage: %v",
+								senderProfile.UserID,
+								senderProfile.DisplayName,
+								senderProfile.PictureURL,
+								senderProfile.StatusMessage,
+							)
+						}
+					}
+
+				} else {
+					//オウム返し
+					sender := getSenderName(c, bot, event.Source.UserID)
+					replyMessage = sender + "さんの発言:\n" + message.Text
+				}
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+					log.Errorf(c, "Error occurred at reply-message. err: %v", err)
+				}
+			}
+
