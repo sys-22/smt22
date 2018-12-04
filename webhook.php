71979625da3cd2955f9ebcfb9f268b335b07e8a1
suzuki <smt.sys-22@sysmet.co.jp>
2018-12-04 16:37:56 +0900

    make it better

--- webhook.php
+++ webhook.php
@@ -1,7 +1,7 @@
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('mREkOWlkv8FLyzekkvjSLa7peMd0ZXsQlRlTdfPGf83I1/L8agaoMUkKsmHOdree39U4VwZV4VMfe49EycwgTa9Rg8xlNnk4rGh5jlkZdqidra0OEmPSF0mmwtqHnoBklUUhXzczMEVVXLQeRYQ0IQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => fb18e7478baee5c48f1cfad36786270f']);
-$response = $bot->getProfile('<userId>');
+$response = $bot->getProfile('1627227117');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
