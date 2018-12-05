b3374de85fea5990cb2e6c4c2c1cb94915b08a90
suzuki <smt.sys-22@sysmet.co.jp>
2018-12-05 10:40:59 +0900

    コミット

--- webhook.php
+++ webhook.php
@@ -1,48 +1,33 @@
-<?php
-/**
- * Copyright 2016 LINE Corporationww
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-require_once('./LINEBotTiny.php');
-$channelAccessToken = 's2HtA7+7R5R6LBD7KlyP0lSEJUCiS2UgffJU/QyF0kz6PjEcw/yWaQKU4VroIXo039U4VwZV4VMfe49EycwgTa9Rg8xlNnk4rGh5jlkZdqilTuNrC5Dyk/0O51k1jfGquHExFtwLcwESii+ze0g9aAdB04t89/1O/w1cDnyilFU=';
-$channelSecret = 'fb18e7478baee5c48f1cfad36786270f';
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']
-                            )
-                        )
-                    ));
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
-    }
-};
+$postData = array(
+  'grant_type'    => 'authorization_code',
+  'code'          => $_GET['code'],
+  'redirect_uri'  => 'smt22.herokuapp.com/webhook.php',
+  'client_id'     => '1627227117',
+  'client_secret' => 'fb18e7478baee5c48f1cfad36786270f'
+);
 
-?>
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+curl_setopt($ch, CURLOPT_URL, 'https://api.line.me/oauth2/v2.1/token');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$json = json_decode($response);
+$accessToken = $json->access_token;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $accessToken));
+curl_setopt($ch, CURLOPT_URL, 'https://api.line.me/v2/profile');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$json = json_decode($response);
