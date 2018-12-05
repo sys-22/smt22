997ee3cf6a92a7aede9aa2b992d6a18c5c911686
suzuki <smt.sys-22@sysmet.co.jp>
2018-12-05 11:20:55 +0900

    make it better

--- webhook.php
+++ webhook.php
@@ -1,11 +1,11 @@
-<?php
-
+<html>
 <a href="https://access.line.me/oauth2/v2.1/authorize
             ?response_type=code
             &client_id= 1627227117
             &redirect_uri=https://smt22.herokuapp.com/webhook.php
             &state=—”
             &scope=profile">LINEƒƒOƒCƒ“‚·‚é</a>
+<?php
 
 $postData = array(
   'grant_type'    => 'authorization_code',
@@ -42,3 +42,4 @@ curl_close($ch);
 $json = json_decode($response);
 
 ?>
+</html>
